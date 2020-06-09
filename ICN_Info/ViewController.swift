52dd4adba4d3e2d3e6b0d1028282c5128cedfc89
MoNireu <monireu.dev@gmail.com>
2020-06-09 20:19:04 +0900

    Initial Commit

---
+++ ICN_Info/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ICN_Info
+//
+//  Created by MoNireu on 2020/06/09.
+//  Copyright © 2020 monireu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
